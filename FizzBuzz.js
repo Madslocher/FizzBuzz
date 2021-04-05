7cdc51b0ea532f31cf764328777bd3da9ec34eb9
Madslocher <Madslocher@hotmail.com>
2021-04-05 13:20:20 +0200

    Comments
    Comments in the code, and source in readme

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,12 +1,20 @@
-// Game FizzBuzz
+/*
+	FizzBuzz is a game designed to teach division, players take turn to count from one and up, 
+	replacing any number dvisible by the number three with the word "fizz" and any number divisible by five with "Buzz".
+	If the number is a divisible of three and five it is replaced with "FizzBuzz".
+*/
+for(var i = 1; i<=100; i++){					//Loop to initiate the game.
 
-//Loop from 1 to 100
-for(var i = 1; i<=100; i++){
-    if (i % 15 == 0){console.log("FizzBuzz")}
+
+	/*
+		if/else statement here: use % to get remainder after division.
+	*/
+
+    if (i % 15 == 0){console.log("FizzBuzz")}		//"FizzBuzz" has to be on top of the loop, as not to trigger three times.
 
     else if (i % 5 == 0){console.log("Buzz")}
 
     else if (i % 3 == 0){console.log("Fizz")}
 
-    else (console.log(i))
+    else (console.log(i))							//If no division with a remainder of 0 was found, print and start over.
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # FizzBuzz
- Game of numbers
+ Game of division
 
 Count up and print from 1.
 Numbers that are multipication of 3 is replaced by the word Fizz
@@ -9,4 +9,5 @@ Numbers that are multipication of 3 and 5 is replaced by the word FizzBuzz
 Easy...
 
 
-Based on a Tom Scott video, https://youtu.be/QPZ0pIK_wsc
+Based on a Tom Scott video: https://youtu.be/QPZ0pIK_wsc
+Further reference: https://en.wikipedia.org/wiki/Fizz_buzz
